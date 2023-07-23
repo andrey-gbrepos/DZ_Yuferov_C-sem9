1e7342886874885398a8af71830c4b54901dc11c
andrey-gbrepose <yandire@yandex.ru>
2023-07-23 07:14:52 +0300

    Zadacha66

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая 
+//найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+using System;
+using static System.Console;
+
+Clear();
+WriteLine("Программа, с помощью рекурсии, выводит все натуральные числа в промежутке"); 
+WriteLine("от одного заданного числа до другого");
+WriteLine();
+Write("Задайте первое натуральное число: ");
+int num1 = int.Parse(ReadLine());
+
+Write("Задайте второе натуральное число: ");
+int num2 = int.Parse(ReadLine());
+
+WriteLine(OutNaturalNum (num1, num2));
+
+string OutNaturalNum (int numA, int numB)
+{ 
+    if(numA <= numB)
+    {
+    if (numA == numB)
+        {
+        return Convert.ToString(numB);
+        }
+        else
+        {    
+        return ($"{numA} {OutNaturalNum(numA+1, numB)}");    
+        }
+    } 
+    else
+    {
+        if (numA == numB)
+        {
+        return Convert.ToString(numB);
+        }
+        else
+        {    
+        return ($"{numA} {OutNaturalNum(numA-1, numB)}");    
+        }
+    } 
+}
